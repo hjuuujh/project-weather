e71865b54346a4d3ec1343cfc0c5c017c3795a1d
hjuuujh <hjuuujh@gmail.com>
2024-07-01 19:54:45 +0900

    project weather crud api

--- src/main/java/zerobase/projectweather/ProjectWeatherApplication.java
+++ src/main/java/zerobase/projectweather/ProjectWeatherApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-
+@EnableTransactionManagement
+@EnableScheduling
 public class ProjectWeatherApplication {
 
     public static void main(String[] args) {
